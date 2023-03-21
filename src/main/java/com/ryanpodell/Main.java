4dbe8018a9a4e3f267104a4705e09f070cfd2e63
rodell <ryanodell2668@gmail.com>
2023-03-21 16:33:16 -0400

    Copy of files and directories complete

--- src/main/java/com/ryanpodell/Main.java
+++ src/main/java/com/ryanpodell/Main.java
@@ -1,19 +1,10 @@
 package com.ryanpodell;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.DirectoryIteratorException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-
         //String fromFolder = "C:\\Users\\rpode\\OneDrive\\Desktop\\TestFrom";
         //String toFolder = "C:\\Users\\rpode\\OneDrive\\Desktop\\TestTo\\";
         //Files.copy(Path.of(fromFolder), Path.of(toFolder));
@@ -30,24 +21,27 @@ public class Main {
             System.err.println(x);
         }
 
-        for(Object file : filesToCopy){
-            System.out.println(file);
-        }
-
         //PASTE
         String toFolder = "C:\\Users\\rpode\\OneDrive\\Desktop\\TestTo\\";
         String fileName = "";
         for(Object file : filesToCopy){
             fileName = file.toString().substring(file.toString().lastIndexOf('\\') + 1);
-            System.out.println(fileName);
+            Path sourceFilePath = Path.of(file.toString());
+            Path targetFilePath = Path.of(toFolder + fileName);
+
+            if (!Files.exists(targetFilePath)){
+                Files.copy(sourceFilePath, targetFilePath);
+            } else {
+                //Future releases of this software to allow for option to copy file or skip
+                //Files.copy(sourceFilePath, targetFilePath, StandardCopyOption.REPLACE_EXISTING);
+                //System.out.println("Replaced " + fileName + " with an updated file");
+                System.out.println("File " + fileName + " already exists. Skipping to next file.");
+            }
         }
 
-
-        System.out.println("Program complete.");
-
+        System.out.println("\nProgram complete.");
     }
 }
-
 
 //  "C:\\Users\\rpode\\OneDrive\\Desktop\\TestFrom\\"
 //  "C:\\Users\\rpode\\OneDrive\\Desktop\\TestTo\\"
