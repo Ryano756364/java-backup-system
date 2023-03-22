39ecd51d428adca4b3786b6ab2fa9c60074e2c83
rodell <ryanodell2668@gmail.com>
2023-03-22 12:24:37 -0400

    Program functional with manual application

--- src/main/java/com/ryanpodell/Main.java
+++ src/main/java/com/ryanpodell/Main.java
@@ -5,7 +5,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 import static java.nio.file.FileVisitResult.*;
 
 public class Main {
-    static Path MASTER_SOURCE_ROOT_PATH = Path.of("C:\\Users\\rpode\\OneDrive\\Desktop\\TestFrom");  //41
+    static Path MASTER_SOURCE_ROOT_PATH = Path.of("C:\\Users\\rpode\\OneDrive\\Desktop\\TestFrom");
     static Path MASTER_DESTINATION_ROOT_PATH = Path.of("C:\\Users\\rpode\\OneDrive\\Desktop\\TestTo\\");
 
     public static void main(String[] args){
@@ -25,35 +25,27 @@ public class Main {
         }
     }
 
+    //Need to return and build directories first, then files
     public static class AnalyzeCurrentPath extends SimpleFileVisitor<Path> {
-        // Print information about each type of file.
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
-            //System.out.println(file);
             if (attr.isDirectory()) {
-                //System.out.println("Directory");
-                //System.out.println(String.valueOf(file).substring(41));
                 manageDirectory(file);
                 shipDirectoryPath(file);
             } else {
-                //System.out.println("Not Directory");
-                //shipFilePath(file);
+                shipFilePath(file);
             }
-            System.out.println("");
             return CONTINUE;
         }
     }
 
     public static void shipFilePath(Path pathToShip) throws IOException {
+        String parsedPath = String.valueOf(pathToShip).substring(41);
+        Path targetFilePath = Path.of(MASTER_DESTINATION_ROOT_PATH + "\\" + parsedPath);
 
-        String targetString = String.valueOf(MASTER_DESTINATION_ROOT_PATH) + String.valueOf(pathToShip).substring(41);
-        Path targetFilePath = Path.of(targetString);
         if (!Files.exists(targetFilePath)){
             Files.copy(pathToShip, targetFilePath);
         } else {
-            //Future releases of this software to allow for option to copy file or skip
-            //Files.copy(sourceFilePath, targetFilePath, StandardCopyOption.REPLACE_EXISTING);
-            //System.out.println("Replaced " + fileName + " with an updated file");
             System.out.println("File " + pathToShip.getFileName() + " already exists. Skipping to next file.");
         }
     }
@@ -64,32 +56,3 @@ public class Main {
         Files.createDirectories(dir);
     }
 }
-
-/*
-        //COPY
-        ArrayList<Object> filesToCopy = new ArrayList<>();
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(pathToShip)) {
-            for(Path file: stream){
-                filesToCopy.add(file);
-            }
-        } catch (IOException | DirectoryIteratorException x) {
-            System.err.println(x);
-        }
-
-        //PASTE
-        String fileName = "";
-        for(Object file : filesToCopy){
-            fileName = file.toString().substring(file.toString().lastIndexOf('\\') + 1);
-            Path sourceFilePath = Path.of(file.toString());
-            Path targetFilePath = Path.of(destinationPath + fileName);
-
-            if (!Files.exists(targetFilePath)){
-                Files.copy(sourceFilePath, targetFilePath);
-            } else {
-                //Future releases of this software to allow for option to copy file or skip
-                //Files.copy(sourceFilePath, targetFilePath, StandardCopyOption.REPLACE_EXISTING);
-                //System.out.println("Replaced " + fileName + " with an updated file");
-                System.out.println("File " + fileName + " already exists. Skipping to next file.");
-            }
-        }
-        */
